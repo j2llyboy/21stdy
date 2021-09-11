e6e3097669e6c19684847e06b8d3a31156df94ef
j2llyboy <j2llyboy@gmail.com>
2021-09-11 17:32:38 +0900

    123

--- Project001/src/Pack/Fourfundarulescalcul.java
+++ Project001/src/Pack/Fourfundarulescalcul.java
@@ -7,9 +7,10 @@ public class Fourfundarulescalcul {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-	System.out.println("»çÄ¢¿¬»ê Å¸ÀÔÀ» ÀÔ·Â ex) µ¡¼À »¬¼À °ö¼À ³ª´°¼À");	
+	System.out.println("1. »çÄ¢¿¬»ê Å¸ÀÔÀ» ÀÔ·ÂÇÏ¼¼¿ä");
+	System.out.println("ex) µ¡¼À(+) »¬¼À(-) °ö¼À(*) ³ª´°¼À(/)");
 	Scanner scty = new Scanner(System.in);
-	System.out.println("¼ýÀÚ µÎ°³ ÀÔ·Â ½Ã »çÄ¢¿¬»ê °á°ú°ª Ãâ·Â");
+	System.out.println("2. ¼ýÀÚ µÎ°³¸¦ ÀÔ·Â ½Ã »çÄ¢¿¬»ê °á°ú°ªÀ» Ãâ·ÂÇÕ´Ï´Ù.");
 	Scanner sc = new Scanner(System.in);
 	Scanner sc1 = new Scanner(System.in);
 	
@@ -20,11 +21,11 @@ public class Fourfundarulescalcul {
 	// && : and, || : or
 	if (scty1.equals("µ¡¼À") || scty1.equals("+")) {
 		System.out.println("a + b = " + (a + b) );
-	} else if( scty1.equals ("»¬¼À")){ 
+	} else if( scty1.equals ("»¬¼À") || scty1.equals("-")){ 
 		 System.out.println("a - b = " + (a - b) );
-	} else if ( scty1.equals ("°ö¼À")){ 
+	} else if ( scty1.equals ("°ö¼À") || scty1.equals("*")){ 
 		 System.out.println("a * b = " + (a * b) );
-	} else if ( scty1.equals ("³ª´°¼À")){ 
+	} else if ( scty1.equals ("³ª´°¼À") || scty1.equals("/")){ 
 		 System.out.println("a / b = " + (a / b) );
 	} else {
 		System.out.println("»çÄ¢¿¬»êÁß¿¡¼­ ÀÔ·ÂÇÏ¼¼¿ä");
